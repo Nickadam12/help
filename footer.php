120ff8f95444fdb5d57f44d5015826a7f14efbe1
Laravel Coader <krishna@nysoftech.com>
2023-09-05 18:19:23 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -24,4 +24,12 @@ $footer_nav_menu = wp_nav_menu( [
 			?>
 		</nav>
 	<?php endif; ?>
+	<?php if ( $footer_nav_menu ) : ?>
+		<nav class="site-navigation">
+			<?php
+			// PHPCS - escaped by WordPress with "wp_nav_menu"
+			echo $footer_nav_menu; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			?>
+		</nav>
+	<?php endif; ?>
 </footer>
